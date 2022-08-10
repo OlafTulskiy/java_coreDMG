48f7447b8824e44e141ceffc17982fc838318d90
m.dvoryankin <m.dvoryankin@npo-pkrv.ru>
2022-08-10 15:46:35 +0300

    DZ from 9 lesson is done

---
+++ src/com/company/lesson9DZ/Student.java
@@ -0,0 +1,23 @@
+package com.company.lesson9DZ;
+
+import java.util.List;
+
+
+public class Student {
+    private String name;
+    List<Course> courseList;
+
+    public Student(String name, List<Course> courseList) {
+        this.name = name;
+        this.courseList = courseList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+}
